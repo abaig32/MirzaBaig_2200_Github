1edbc8fc202484b10553346f268c3d54e88d550f
Mirza Baig <mirzab23@outlook.com>
2022-11-14 10:28:40 -0600

    Push of PE6.4

---
+++ MirzaBaig_2200_PE6.4/src/Exercise06_04.java
@@ -0,0 +1,39 @@
+/*****
+ * 
+ * @author Mirza Baig 
+ * Date: 11/14/2022 [M]
+ *
+ */
+
+import java.util.Scanner; 
+public class Exercise06_04 {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter an integer: ");
+		int number = input.nextInt();
+		reverse(number);
+		
+		
+		input.close();
+
+	}
+	
+	public static void reverse(int n) {
+		if (n == 0) {
+			
+			System.out.print(n);
+		}
+		
+		while( n != 0) {
+			
+			System.out.print(n % 10);
+			n = n / 10; 
+			
+		}
+		
+		
+	}
+
+}
